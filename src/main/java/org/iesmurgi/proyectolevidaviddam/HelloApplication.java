505ac42e09db33e2d2dae73e00622bc429de5dc4
lewiscalewis <lewisvicna@gmail.com>
2022-03-16 22:50:22 +0100

    probando estilos en formularios

--- src/main/java/org/iesmurgi/proyectolevidaviddam/HelloApplication.java
+++ src/main/java/org/iesmurgi/proyectolevidaviddam/HelloApplication.java
@@ -4,20 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException, InterruptedException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sign_up.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 300);
         stage.setTitle("Hello!");
         stage.setMaximized(true);
         stage.setMinWidth(700);
         stage.setMinHeight(300);
         stage.setScene(scene);
-
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
 
         stage.show();
         Thread.sleep(500);
--- src/main/java/org/iesmurgi/proyectolevidaviddam/SignUp.java
+++ src/main/java/org/iesmurgi/proyectolevidaviddam/SignUp.java
@@ -1,4 +1,57 @@
 package org.iesmurgi.proyectolevidaviddam;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import org.controlsfx.control.HyperlinkLabel;
+
 public class SignUp {
+    @FXML
+    private Label lblApellidos;
+
+    @FXML
+    private Label lblContraseña;
+
+    @FXML
+    private Label lblCorreo;
+
+    @FXML
+    private Label lblNombre;
+
+    @FXML
+    private Label lblRepetirContraseña;
+
+    @FXML
+    private PasswordField pwdContraseña;
+
+    @FXML
+    private PasswordField pwdRepetirContraseña;
+
+    @FXML
+    private TextField textFieldApellidos;
+
+    @FXML
+    private TextField textFieldCorreo;
+
+    @FXML
+    private TextField textFieldNombre;
+
+    @FXML
+    private VBox vBoxSignUp;
+
+    @FXML
+    private Button btnRegistrarse;
+
+    @FXML
+    void initialize(){
+        lblNombre.getStyleClass().setAll("lbl","lbl-primary");
+        lblApellidos.getStyleClass().setAll("lbl", "lbl-primary");
+        lblCorreo.getStyleClass().setAll("lbl", "lbl-primary");
+        lblContraseña.getStyleClass().setAll("lbl", "lbl-primary");
+        lblRepetirContraseña.getStyleClass().setAll("lbl", "lbl-primary");
+        btnRegistrarse.getStyleClass().setAll("btn", "btn-primary");
+    }
 }
