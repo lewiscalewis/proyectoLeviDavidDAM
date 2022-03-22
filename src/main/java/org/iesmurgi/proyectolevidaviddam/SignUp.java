fec7bfb37d6a6bd63ab67bc8de70c105eda3a589
lewiscalewis <lewisvicna@gmail.com>
2022-03-23 00:26:36 +0100

    subiendo rama local de pruebas levi

--- src/main/java/org/iesmurgi/proyectolevidaviddam/SignUp.java
+++ src/main/java/org/iesmurgi/proyectolevidaviddam/SignUp.java
@@ -1,12 +1,19 @@
 package org.iesmurgi.proyectolevidaviddam;
 
+import com.google.gson.Gson;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import org.controlsfx.control.HyperlinkLabel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SignUp {
     @FXML
@@ -46,6 +53,9 @@ public class SignUp {
     private Button btnRegistrarse;
 
     @FXML
+    private Hyperlink login;
+
+    @FXML
     void initialize(){
 //        lblNombre.getStyleClass().setAll("lbl","lbl-primary");
 //        lblApellidos.getStyleClass().setAll("lbl", "lbl-primary");
@@ -53,5 +63,39 @@ public class SignUp {
 //        lblContraseña.getStyleClass().setAll("lbl", "lbl-primary");
 //        lblRepetirContraseña.getStyleClass().setAll("lbl", "lbl-primary");
         btnRegistrarse.getStyleClass().setAll("btn", "btn-success");
+    }
+
+    @FXML
+    void login(MouseEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("log_in.fxml"));
+            vBoxSignUp.getChildren().clear();
+            vBoxSignUp.getChildren().add(fxmlLoader.load());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void signup(MouseEvent event) throws IOException {
+        String url1 = "https://www.mmobomb.com/api1/games?sort-by=";
+
+        BufferedReader rd;
+        BufferedReader rd2;
+        String linea;
+
+        URL urls1 = new URL(url1);
+        HttpURLConnection conexion1 = (HttpURLConnection) urls1.openConnection();
+        conexion1.setRequestMethod("GET");
+        rd = new BufferedReader(new InputStreamReader(conexion1. getInputStream()));
+
+        // Mientras el BufferedReader se pueda leer, agregar contenido a resultado
+        while ((linea = rd.readLine()) != null) {
+            System.out.println(linea);
+        }
+
+        // Cerrar el BufferedReader
+        rd.close();
     }
 }
