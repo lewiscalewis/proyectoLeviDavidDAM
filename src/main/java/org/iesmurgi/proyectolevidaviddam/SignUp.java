c0534fd753d3a111debbfb75a75a4fb7292dafd1
leví <71761797+lewiscalewis@users.noreply.github.com>
2022-03-23 23:02:07 +0100

    Levi (#3)
    * subiendo rama local de pruebas levi
    
    * añadido una petición con parámetros POST

--- src/main/java/org/iesmurgi/proyectolevidaviddam/SignUp.java
+++ src/main/java/org/iesmurgi/proyectolevidaviddam/SignUp.java
@@ -1,12 +1,20 @@
 package org.iesmurgi.proyectolevidaviddam;
 
+import com.google.gson.Gson;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import org.controlsfx.control.HyperlinkLabel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class SignUp {
     @FXML
@@ -46,6 +54,9 @@ public class SignUp {
     private Button btnRegistrarse;
 
     @FXML
+    private Hyperlink login;
+
+    @FXML
     void initialize(){
 //        lblNombre.getStyleClass().setAll("lbl","lbl-primary");
 //        lblApellidos.getStyleClass().setAll("lbl", "lbl-primary");
@@ -53,5 +64,50 @@ public class SignUp {
 //        lblContraseña.getStyleClass().setAll("lbl", "lbl-primary");
 //        lblRepetirContraseña.getStyleClass().setAll("lbl", "lbl-primary");
         btnRegistrarse.getStyleClass().setAll("btn", "btn-success");
+    }
+
+    @FXML
+    void login(MouseEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("log_in.fxml"));
+            vBoxSignUp.getChildren().clear();
+            vBoxSignUp.getChildren().add(fxmlLoader.load());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void signup(MouseEvent event) throws IOException {
+        String url1 = "http://25.41.23.74:3000/signup";
+
+        BufferedReader rd;
+        String linea;
+
+        String urlParameters  = textFieldCorreo.getText();
+        byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+        int    postDataLength = postData.length;
+
+        URL urls1 = new URL(url1);
+        HttpURLConnection conexion = (HttpURLConnection) urls1.openConnection();
+        conexion.setRequestMethod("POST");
+        conexion.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+
+        rd = new BufferedReader(new InputStreamReader(conexion. getInputStream()));
+
+        // Mientras el BufferedReader se pueda leer, agregar contenido a resultado
+        while ((linea = rd.readLine()) != null) {
+            System.out.println(linea);
+            if(linea.equals("mail_error")){
+                Alert a = new Alert(Alert.AlertType.NONE);
+                a.setAlertType(Alert.AlertType.ERROR);
+                a.setTitle("El correo introducido ya está en uso");
+                a.show();
+            }
+        }
+
+        // Cerrar el BufferedReader
+        rd.close();
     }
 }
