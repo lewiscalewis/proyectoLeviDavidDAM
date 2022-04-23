6ed1b872adce1d1e82ac8d7dc5f30cea6094e58b
leví <71761797+lewiscalewis@users.noreply.github.com>
2022-04-23 22:29:36 +0200

    David vistas (#15)
    * modificando contacts y addfriend para la gestion de contactos del usuario
    
    * menu principal hello view modificada
    
    * filtrado de busqueda de usuarios para agregar a amigos hecho -> siguiente ->
    añadir eventos de solicitudes de amistad
    
    * Requester arreglado.
    
    Co-authored-by: whywo <david.perez.dam2@gmail.com>

--- src/main/java/org/iesmurgi/proyectolevidaviddam/Controllers/LogIn.java
+++ src/main/java/org/iesmurgi/proyectolevidaviddam/Controllers/LogIn.java
@@ -1,6 +1,7 @@
 package org.iesmurgi.proyectolevidaviddam.Controllers;
 
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -23,6 +24,7 @@ import org.iesmurgi.proyectolevidaviddam.Middleware.TokenManager;
 import org.iesmurgi.proyectolevidaviddam.models.User;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
@@ -62,68 +64,90 @@ public class LogIn {
             }
         });
 
-//        btnIniciarSesion.setOnAction(actionEvent -> {
-//
-//            String response = "";
-//
-//            String url = CONSTANT.URL.getUrl()+"/login";
-//            GeneralDecoder md5 = new GeneralDecoder();
-//
-//            try {
-//                ArrayList<String[]> params = new ArrayList<>();
-//                params.add(new String[]{"username", textFieldUsuario.getText()});
-//                params.add(new String[]{"password", md5.encodeMD5(textFieldContrasena.getText())});
-//                OpenThread<String> t = new OpenThread<>(url, params, "POST", String.class);
-//                response = t.getResult();
-//
-//            } catch (IOException | InterruptedException | NoSuchAlgorithmException e) {
-//                e.printStackTrace();
-//            }
-//
-//            System.out.println("Respuesta: "+response);
-//            if(response.equals("login_error") || response.equals("")){
-//                Alert a = new Alert(Alert.AlertType.NONE);
-//                a.setAlertType(Alert.AlertType.ERROR);
-//                a.setTitle("Error de Autentificación");
-//                a.setContentText("El usuario o la contraseña son incorrectos");
-//                a.show();
-//            }else{
-//                //Llamamos al gestor del token para que guarde localmente el token
-//                TokenManager tkm = new TokenManager();
-//                tkm.tokenStorage(response);
-//                System.out.println("Token de usuario: "+response);
-//            }
-//
-//
-//            scene =btnIniciarSesion.getScene();
-//            stage = (Stage) scene.getWindow();
-//            try {
-//                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//                Parent helloView = fxmlLoader.load() ;
-//                HelloController helloController = fxmlLoader.getController();
-//                helloController.loadHomePage(); //Loads Home page
-//
-//
-//
-//               GeneralDecoder gd = new GeneralDecoder();
-//
-//
-//                String username= gd.getUserFromToken();
-//
-//
-//                Requester<User[]> userRequester = new Requester<>("http://tux.iesmurgi.org:11230/user",Requester.Method.POST,User[].class);
-//                userRequester.addParam("username",username);
-//                helloController.loadUserData(userRequester.execute()[0]);
-//
-//                Scene s = new Scene(helloView, scene.getWidth(), stage.getHeight()-34, Color.BLACK);
-//                stage.setScene(s);
-//                stage.show();
-//            } catch (Exception e){
-//                e.printStackTrace();
-//            }
-//        }
+        btnIniciarSesion.setOnAction(actionEvent -> {
+
+            String response = "";
+
+            String url = CONSTANT.URL.getUrl()+"/login";
+            GeneralDecoder md5 = new GeneralDecoder();
+
+            try {
+                ArrayList<String[]> params = new ArrayList<>();
+                params.add(new String[]{"username", textFieldUsuario.getText()});
+                params.add(new String[]{"password", md5.encodeMD5(textFieldContrasena.getText())});
+                OpenThread<String> t = new OpenThread<>(url, params, "POST", String.class);
+                response = t.getResult();
+
+            } catch (IOException | InterruptedException | NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Respuesta: "+response);
+            if(response!=null){//////////////////////////////////////////////////////SALE SI LA RESPUESTA ES NULL
+                if(response.equals("login_error") || response.equals("")){
+                    Alert a = new Alert(Alert.AlertType.NONE);
+                    a.setAlertType(Alert.AlertType.ERROR);
+                    a.setTitle("Error de Autentificación");
+                    a.setContentText("El usuario o la contraseña son incorrectos");
+                    a.show();
+                }else{
+                    //Llamamos al gestor del token para que guarde localmente el token
+                    TokenManager tkm = new TokenManager();
+                    tkm.tokenStorage(response);
+                    System.out.println("Token de usuario: "+response);
+                }
 
 
+
+            scene =btnIniciarSesion.getScene();
+            stage = (Stage) scene.getWindow();
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+                Parent helloView = fxmlLoader.load() ;
+                HelloController helloController = fxmlLoader.getController();
+                helloController.loadHomePage(); //Loads Home page
+
+
+
+               GeneralDecoder gd = new GeneralDecoder();
+
+
+                String username= gd.getUserFromToken();
+
+
+                Platform.runLater(()->{
+                    Requester<User[]> userRequester = null;
+                    try {
+                        userRequester = new Requester<>("http://tux.iesmurgi.org:11230/user",Requester.Method.POST, User[].class);
+                        userRequester.addParam("username",username);
+                        helloController.loadUserData(userRequester.execute()[0]);
+
+                        Scene s = new Scene(helloView, scene.getWidth(), stage.getHeight()-34, Color.BLACK);
+                        stage.setScene(s);
+                        stage.show();
+
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });/*
+                Thread loginRequestThread= new Thread(()->{
+
+
+                });
+                loginRequestThread.setDaemon(true);
+                loginRequestThread.start();
+*/
+
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+
+                }
 /*
                 try {
                     FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("homepage.fxml"));
