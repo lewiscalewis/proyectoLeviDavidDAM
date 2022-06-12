2719a5746a9b6ae9ed5167c59a8fe1e0f7f9e53b
DavidPerezContreras <91068383+DavidPerezContreras@users.noreply.github.com>
2022-06-12 16:09:44 +0200

    Comentarios Modelo FriendRequest

--- src/main/java/org/iesmurgi/proyectolevidaviddam/models/FriendRequest.java
+++ src/main/java/org/iesmurgi/proyectolevidaviddam/models/FriendRequest.java
@@ -1,10 +1,15 @@
 package org.iesmurgi.proyectolevidaviddam.models;
 
+
+//Modelo de una Petición de amistad
 public class FriendRequest {
 
-    int id;
-    String emisor, receptor, state;
-
+    int id;     //request id
+       
+    String emisor;  //Usuario que envía la petición
+    String receptor;    //Usuario a quien es enviada la petición
+    String state;   //Para saber si la petición ha sido aceptada, denegada o aun sigue pendiente.
+    
     public FriendRequest(int id, String emisor, String receptor, String state){
         this.id = id;
         this.emisor = emisor;
